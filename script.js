966d7fe33780cc93ad243a35da058cccbe577832
Gabriel Camaleonte <162625497+GabrielCamaleonte@users.noreply.github.com>
2024-04-15 14:46:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(frase) {
+    frase = frase.toLowerCase().replaceAll(" ", "");
+    let fraseInvertida = "";
+    for (let i = frase.length - 1; i >= 0; i--) {
+        fraseInvertida += frase[i];
+    }
+    if (fraseInvertida == frase) {
+        return true
+    }
+    return false
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(array) {
+    let max = -Infinity;
+    let min = Infinity;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return [min,max];
 }
